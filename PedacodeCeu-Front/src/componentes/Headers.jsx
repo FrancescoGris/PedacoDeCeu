789208f91cf954a83db1fe4aed9e3d3651563fc5
Francesco Gris <francescofgris@gmail.com>
2026-06-25 21:39:01 -0300

    feat: substituir logo por versão com fundo transparente

--- PedacodeCeu-Front/src/componentes/Headers.jsx
+++ PedacodeCeu-Front/src/componentes/Headers.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import logoImg from '../ativos/Logos/clara-img.png';
+import logoImg from '../ativos/Logos/clara-transparente-img.png';
 import { useAuth } from '../context/AuthContext';
 import { useCarrinho } from '../context/CarrinhoContext';
 import './Headers.css';
